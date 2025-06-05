39bc1ae7a80deeacfa92be5e08f2e94c35a26ef6
Michael-004 <grandamicha20@gmail.com>
2025-06-05 14:49:26 -0500

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login } from '../auth/authCtrl';
+import { login } from '../auth/authCtrl.js';
 const router = express.Router();
 router.post('/login', login);
 export default router;
