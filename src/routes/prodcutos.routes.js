4a0562860e6017b627a118f68247763e5ba2bbd4
Michael-004 <grandamicha20@gmail.com>
2025-06-05 14:44:51 -0500

    Update prodcutos.routes.js

--- src/routes/prodcutos.routes.js
+++ src/routes/prodcutos.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { registrarProducto, registrarTalla } from '../controladores/prodcutoCtrl.js';
+import { registrarProducto, registrarTalla } from '../controladores/prodcutosCtrl.js';
 import { subirImagenes } from '../controladores/imagenCtrl.js';
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
